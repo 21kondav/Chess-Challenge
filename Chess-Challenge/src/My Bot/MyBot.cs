3f80d754b413660836e6fec27558e32cb4d0aa19
21kondav <21kondav@gmail.com>
2023-08-23 18:04:55 -0400

    update with minimax

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,62 +5,27 @@ public class MyBot : IChessBot
 {
     //todo: dyanmic bit boards
     //TODO: hotspot grid for pieves given a bit board set up
-    private static double[] piece_vals = {1,  3, 3.2, 5, 9}; //follows the same structure as PieceType enum
+    private static decimal[] piece_vals = {1, 3, 3.2m, 5, 9}; //follows the same structure as PieceType enum
     public Move Think(Board board, Timer timer)
     {
         bool isWhite = board.IsWhiteToMove;
         //to remove in final 
         //set up necessary vars
         Move[] moves = board.GetLegalMoves();
-        Random rand = new Random();
-        int ply = board.PlyCount;
+
 
         MoveEval no_move = new MoveEval(board);
+        MoveEval alpha = new MoveEval(board, true);
+        MoveEval beta = new MoveEval(board, false);
         MoveTree move_tree = new MoveTree(no_move, 4);
         move_tree.addChildren(move_tree);
-        move_tree.GetPaths(move_tree, move_tree.AddToList);
-        // move_tree.PurgeTree();
-                //evaluate board piece
-        LinkedList<MoveEval> best = move_tree.paths[0];
-        double bestVal = MoveEval.PathEval(best);
-        foreach(LinkedList<MoveEval> path in move_tree.paths){
-            if(path.Count < 2) continue;
-            double val =  MoveEval.PathEval(path);
-            if(isWhite){
-                if(val > bestVal){
-                    best = path;
-                    bestVal = val;
-                }
-            }else{
-                if(val < bestVal){
-                    best = path;
-                    bestVal = val;
-                }                
-            }
 
-        }
-        return best.First.Next.Value.move;
-
-    }
-    //weighting pieces valuability
-    public static double gaussian(int x, int y){
-        double total = 0;
-        double exp = (Math.Pow(x-4, 2) + Math.Pow(y-4, 2))*.5;
-        double board_prob = .2*Math.Exp(-exp/2);
-        total += board_prob;
-        return total;
-    }
-
-    public static LinkedList<MoveEval> AddToList(MoveEval node, LinkedList<MoveEval> path){
-        if (path == null) path = new LinkedList<MoveEval>();
         
-        if (path.Count == 0 || path.Last.Value.depth == node.depth - 1)
-        {        
-            path.AddLast(node);//add to path
-        }
 
-        return path;
+        return move_tree.Minimax(alpha, beta).move;
+
     }
+
     public delegate void DoSomething<MoveTree, LinkedList>(MoveTree node, LinkedList<MoveEval> path);
     public class MoveTree{
         public MoveEval data;
@@ -100,34 +65,35 @@ public class MyBot : IChessBot
             }
             
         }
-        public void AddToList(MoveTree node, LinkedList<MoveEval> path)
-        {
-          while(path.Count > node.data.depth){
-            path.RemoveLast();
-          }
-          if (path.Count == 0 || path.Last.Value.depth == node.data.depth - 1)
-          {        
-            path.AddLast(node.data);//add to path
-            if(node.children.Count == 0){
-              this.paths.Add(new LinkedList<MoveEval>(path));
+
+        public MoveEval Minimax(MoveEval alpha, MoveEval beta ){
+            //if we hit bottom of tree or no more legal moves
+
+            if(this.max_height == 0 || this.data.board.GetLegalMoves().Length == 0)
+                return this.data;
+            
+            MoveEval extreme = new MoveEval(this.data.board, this.data.colorToMove);
+            foreach(MoveTree tree in this.children){
+                MoveEval eval = tree.Minimax(alpha, beta);
+                extreme = extreme.Compare(tree.data);
+                if(this.data.colorToMove)
+                    alpha = alpha.Compare(eval);
+                else
+                    beta = beta.Compare(eval);
+                    
+                if(beta.eval <= alpha.eval) break;
+                
             }
-          }
-        }
-        public void GetPaths(MoveTree node, DoSomething<MoveTree, LinkedList<MoveEval>> visit, LinkedList<MoveEval> path = null)
-        {
-            if(path ==null) path = new LinkedList<MoveEval>();
-            visit(node, path);
-            foreach (MoveTree kid in node.children)
-            {   
-                GetPaths(kid, visit, path);
-            }
-        }
+            return extreme;
+            
+        }   
+
     }
     //general move eval for either side
     public struct MoveEval{
         public Move move;
         public Board board; //stores the board AFTER move
-        public double eval;
+        public decimal eval;
         public bool colorToMove;
         public int depth;
         //takes a board before a move and the move to be made
@@ -139,52 +105,96 @@ public class MyBot : IChessBot
             board = b;
             eval = 0;
             this.depth = depth;
+            decimal pieceEval = 0;
+            decimal board_eval = 0;
             //pieces evaluation
+            if(b.IsInCheckmate()) board_eval += -decimal.MaxValue;
+            board_eval += b.IsInCheck() ? -5 : 0;
             PieceList[] onBoard = board.GetAllPieceLists();
-            double pieceEval = 0;
-            for(int i = 0; i < 11; i++){
-                if(i == 5){
-                    continue;
-                }
-                double extra_value = 0;
-                if(onBoard[i].TypeOfPieceInList == PieceType.Pawn){
-                    foreach(Piece p in onBoard[i]){
-                        extra_value += MyBot.gaussian(p.Square.File, p.Square.Index);
-                    }
-                    
-                }else if(onBoard[i].TypeOfPieceInList == PieceType.Knight){
-                    foreach(Piece p in onBoard[i]){
-                        extra_value += MyBot.gaussian(p.Square.File, p.Square.Rank);
-                    } 
-                }
+            
+            foreach(PieceList p_list in onBoard){
+                Console.WriteLine((int)p_list.TypeOfPieceInList);
+                pieceEval += p_list.TypeOfPieceInList != PieceType.King? (p_list.IsWhitePieceList
+                ? piece_vals[(int)p_list.TypeOfPieceInList- 1]: 
+                    -piece_vals[(int)p_list.TypeOfPieceInList-1]):0;
+                
                 //if threat of caputre, reduce evaluation
-                if(!onBoard[i].IsWhitePieceList){
-                    pieceEval -= (extra_value + onBoard[i].Count*piece_vals[i-6]);
-                }else{
-                    pieceEval += extra_value + onBoard[i].Count*piece_vals[i];
-                }
-            }
-        
-            //add piece eval to total 
-            eval += pieceEval;
 
+            }
+            //add piece eval to total 
+            eval += pieceEval+board_eval;
+            //generally we avoid check and mate
             b.UndoMove(m);
         }
-        public MoveEval(Board b){
+
+        public MoveEval(Board b, bool? isMax= null){
             board = b;
             colorToMove = b.IsWhiteToMove;
             move = Move.NullMove;
-            eval = 0;
             depth = 0;
+            eval = isMax == null ? (colorToMove ?  -Decimal.MaxValue : Decimal.MaxValue): 
+                    ((bool)isMax ? -Decimal.MaxValue : Decimal.MaxValue);
+            
         }
-        public static double PathEval(LinkedList<MoveEval> path){
-
-            double sum = 0;
-            foreach(MoveEval move in path){
-                sum += move.eval;
+        public MoveEval Compare(MoveEval otherMove){
+            if(this.colorToMove){
+                if(otherMove.eval > this.eval) return otherMove;
+                return this;
             }
-            return sum;
+            if(otherMove.eval < this.eval) return otherMove;
+            return this;
         }
- 
+        
     }
+        //weighting pieces valuability
+    //shouldn't matter piece color
+    // public static decimal piece_eval(Piece p, Board board){
+    //     int home_rank = board.GetKingSquare(board.IsWhiteToMove).Rank; 
+    //     double b = 0; //shift up and down
+    //     double a = .5; //amplitude
+    //     double sigma = 2; //spread
+    //     double xshift = 0; //shift along x axis
+    //     double yshift = 0; //shift allong y
+    //     double p_eval = 0;
+    //     //roughly 40 moves per chess game
+    //     int game_phase;
+    //     sigma = Math.Log(board.PlyCount+6); //limit of this is 4.5 with in the chess limits
+    //     if(board.PlyCount < 15) game_phase = 1;
+    //     else if(board.PlyCount < 30) game_phase = 2;
+    //     else game_phase = 3;
+    //     switch(p.PieceType){
+    //         case PieceType.Pawn:
+    //             yshift += Math.Abs(3 - home_rank)*.1;
+    //             if(game_phase == 3) yshift = Math.Abs(9 - home_rank);
+    //             xshift += 4.5;
+    //             p_eval +=  1;
+    //             break;
+    //         case PieceType.Knight:
+    //             //approx opening game                
+    //             yshift += 4.5;
+    //             xshift += 4.5;
+    //             if(game_phase == 1) yshift = Math.Abs( 4.5-home_rank)*.1;
+
+    //             b = -.05;
+    //             p_eval +=3 ;
+    //             break;
+    //         case PieceType.Bishop:
+    //             p_eval += 3.2;
+    //             break;
+    //         case PieceType.Rook:
+    //             p_eval += 5;
+    //             break;
+    //         case PieceType.Queen:
+    //             p_eval += 9;
+    //             break;
+    //         case PieceType.King:
+
+    //             break;
+    //     }
+        
+    //     double exp = (Math.Pow(p.Square.File-xshift, 2) + Math.Pow(p.Square.Rank-yshift, 2))/Math.Pow(sigma, 2);
+    //     p_eval += a*Math.Exp(-.5*exp) + b;
+    //     return (decimal)p_eval;
+    // }
+
 }
